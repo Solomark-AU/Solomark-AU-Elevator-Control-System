68998e2f73914fd04eda202f7f3774148299b886
Shanyu1218 <fby23115@qq.com>
2024-07-05 20:54:38 +0800

    调度模块1.0完成，引入jsoncpp库 位于分支 main 要提交的变更： 	删除：     Algorithm 	修改：     Algorithm.cpp 	新文件：   configure.json 	新文件：   jsoncpp/json/json-forwards.h 	新文件：   jsoncpp/json/json.h 	新文件：   jsoncpp/jsoncpp.cpp
    by Herobrine6265
    Co-authored-by: Link是个赶路人 <LinkDevCodes@users.noreply.github.com>

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -2,13 +2,16 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include "jsoncpp/json/json.h"
 using namespace std;
+using namespace Json;
 
-const int MAXFLOORNUM = 10000, HIGHOFFLOOR = 50, SPEED = 1;
-int FLOOR = 15;
+const int MAXFLOORNUM = 10000, MAXELEVATORNUM = 10;
+int FLOOR, ELEVATORNUM, HighOfFloor, Speed;
 
 int requestedNum[MAXFLOORNUM][7][25][65]; // Count by minute and allows a inaccuracy of ±5 mins;
-int OldStation[7], LastUpDate = 0;
+int station[25][65][MAXELEVATORNUM][7];   //
+int LastUpDate = -1;
 
 enum STATUS
 {
@@ -22,28 +25,57 @@ struct REQUEST
     int req, tar;
 };
 
+bool cmp_bigger(int a, int b)
+{
+    return a > b;
+}
+
 class Elevator
 {
 public:
+    Elevator(int id) { this->ID = id; }
+
+    ~Elevator() {}
+
     void add_target(int req, int tar)
     {
         time_t t;
         time(&t);
-        struct tm *time_info = localtime(&t);
+        tm *time_info = localtime(&t);
         if (time_info->tm_wday != LastUpDate)
         {
             for (int i = 0; i < 25; i++)
                 for (int j = 0; j < 61; j++)
                 {
+                    vector<int> reqNum;
                     for (int k = 1; k < MAXFLOORNUM; k++)
+                    {
+                        reqNum.push_back(requestedNum[k][time_info->tm_wday][i][j]);
                         requestedNum[k][time_info->tm_wday][i][j] = 0;
-                    requestedNum[OldStation[LastUpDate]][LastUpDate][i][j] = 100;
-                    requestedNum[FLOOR / 2][LastUpDate][i][j] = 30;
+                    }
+                    sort(reqNum.begin(), reqNum.end(), cmp_bigger);
+                    for (int k = 1; k <= ELEVATORNUM && k <= 10; k++)
+                    {
+                        requestedNum[station[i][j][k][time_info->tm_wday]][time_info->tm_wday][i][j] += 11 - k;
+                        station[i][j][k][time_info->tm_wday] = reqNum[k - 1];
+                    }
+                    requestedNum[FLOOR / 2][time_info->tm_wday][i][j] += 3;
                 }
             LastUpDate = time_info->tm_wday;
         }
-        for (int i = time_info->tm_min - 5; i <= time_info->tm_min + 5; t++)
-            requestedNum[req][LastUpDate][time_info->tm_hour + (60 + i) / 60 - 1][(60 + i) % 60]++;
+        for (int i = -5; i <= 5; t++)
+        {
+            t -= i * 60;
+            tm *temp = localtime(&t);
+            requestedNum[req][temp->tm_wday][temp->tm_hour][temp->tm_min]++;
+            vector<int> reqNum;
+            for (int k = 1; k < MAXFLOORNUM; k++)
+                reqNum.push_back(requestedNum[k][temp->tm_wday][temp->tm_hour][temp->tm_min]);
+            sort(reqNum.begin(), reqNum.end(), cmp_bigger);
+            for (int k = 1; k <= ELEVATORNUM && k <= 10; k++)
+                station[temp->tm_hour][temp->tm_min][k][time_info->tm_wday] = reqNum[k - 1];
+            t += i * 60;
+        }
         // push into vector
         this->target.push_back(REQUEST{req, tar});
         if ((req - this->floor) * this->status > 0)
@@ -68,21 +100,12 @@ public:
 
     void move()
     {
-        bool Not_Opened = 1;
-        this->high += this->status * SPEED;
-        this->floor = this->high / HIGHOFFLOOR + 1;
+        bool Not_Opened = true;
+        this->high += this->status * Speed;
+        this->floor = this->high / HighOfFloor + 1;
         for (int k = 0; k < this->target.size(); k++)
         {
             REQUEST i = this->target[k];
-            if (i.req == this->floor)
-            {
-                i.req = i.tar;
-                if (Not_Opened)
-                {
-                    this->open_door();
-                    Not_Opened = false;
-                }
-            }
             if (i.req == i.tar && this->floor == i.tar)
             {
                 this->target.erase(this->target.begin() + k);
@@ -93,8 +116,50 @@ public:
                     Not_Opened = false;
                 }
             }
+            if (i.req == this->floor)
+            {
+                i.req = i.tar;
+                if (Not_Opened)
+                {
+                    this->open_door();
+                    Not_Opened = false;
+                }
+            }
         }
-        //Change the Way
+        // Change the Way
+        if ((this->floor == this->lowest && this->status == STATUS::DOWNSIDE) ||
+            (this->floor == this->highest && this->status == STATUS::UPSIDE))
+        {
+            this->highest = 0;
+            this->lowest = MAXFLOORNUM;
+            for (REQUEST i : this->target)
+            {
+                this->highest = max(this->highest, max(i.req, i.tar));
+                this->lowest = min(this->lowest, min(i.req, i.tar));
+            }
+            if (this->status == STATUS::UPSIDE)
+                this->status == STATUS::DOWNSIDE;
+            else if (this->status == STATUS::DOWNSIDE)
+                this->status == STATUS::UPSIDE;
+            if (this->target.empty())
+            {
+                this->isAvailable = true;
+                time_t t;
+                time(&t);
+                tm *temp = localtime(&t);
+                int k = station[temp->tm_hour][temp->tm_min][this->ID][temp->tm_wday];
+                if (this->floor != k)
+                {
+                    this->target.push_back(REQUEST{k, k});
+                    if (k < this->floor)
+                        this->status = STATUS::DOWNSIDE;
+                    else
+                        this->status = STATUS::UPSIDE;
+                }
+                else
+                    this->status = STATUS::STATIC;
+            }
+        }
     }
 
     void open_door() {}
@@ -106,7 +171,7 @@ public:
     STATUS get_status() { return this->status; }
 
 private:
-    int floor = 1, station = 1, high = 0; // station 常驻楼层
+    int floor = 1, high = 0, ID; // station 常驻楼层
     int highest, lowest;
     vector<REQUEST> target;
     STATUS status = STATUS::STATIC;
@@ -115,7 +180,6 @@ private:
 
 int main()
 {
-    cin >> FLOOR;
     while (true)
     {
         int request, target;
